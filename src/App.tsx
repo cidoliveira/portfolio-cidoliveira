0128df36e94d04090fb57e2c8ce2b932f604dda5
Cid Oliveira <alcides.angelo10@gmail.com>
2025-07-24 15:34:45 -0300

    reponsiviness fixes - #5

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,14 @@ function App() {
   return (
     <div>
       <Header />
-      <div className="flex flex-col mx-2 sm:mx-[8vw] lg:mx-[15vw] my-25 sm:my-[8vh] lg:my-[15dvh] font-[Inter] bg-white">
-        <section id="home" className="scroll-mt-50 mb-45">
+      <div className="flex flex-col mx-2 sm:mx-[8vw] lg:mx-[15vw] my-25 sm:my-[8vh] lg:my-[20dvh] font-[Inter] bg-white">
+        <section id="home" className="scroll-mt-70 mb-80">
           <Hero />
         </section>
-        <section id="about" className="scroll-mt-45 mb-45">
+        <section id="about" className="scroll-mt-50 mb-80">
           <About />
         </section>
-        <section id="experience" className="scroll-mt-45 mb-45">
+        <section id="experience" className="scroll-mt-50 mb-80">
           <Experience />
         </section>
       </div>
