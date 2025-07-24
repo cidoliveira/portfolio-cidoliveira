ce06340258ed5c24695e9ff59fca3c27103a1bf2
Cid Oliveira <alcides.angelo10@gmail.com>
2025-07-24 16:18:40 -0300

    bug fixes - #7

--- src/App.tsx
+++ src/App.tsx
@@ -8,16 +8,16 @@ function App() {
   return (
     <div>
       <Header />
-      <div className="flex flex-col mx-2 sm:mx-[10vw] lg:mx-[15vw] my-25 sm:my-[8vh] lg:my-[15dvh] font-[Inter] bg-white">
+      <div className="flex flex-col mx-2 max-sm:m-0 max-sm:mt-10 sm:mx-[10vw] lg:mx-[15vw] my-25 sm:my-[8vh] lg:my-[15dvh] font-[Inter] bg-white">
         <section
           id="home"
-          className="max-sm:mb-0 max-sm:mt-20 scroll-m-60 mt-15 mb-45"
+          className="max-sm:mb-0 max-sm:mt-0 scroll-m-60 mt-15 mb-45"
         >
           <Hero />
         </section>
         <section
           id="about"
-          className="max-sm:mb-0 max-sm:mt-100 scroll-m-60 mt-45 mb-45"
+          className="max-sm:mb-0 max-sm:mt-0 scroll-m-60 mt-45 mb-45"
         >
           <About />
         </section>
